32566240225ff8ec67be8bb1b57a223635fca5e7
Francisco <frankmartinez4050@gmail.com>
2021-08-17 18:54:58 -0500

    Almost done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+function saturdayFun(activity) {
+    if(activity) {
+        return `This Saturday, I want to ${activity}!`;
+    } else return "This Saturday, I want to roller-skate!";
+}
+
+function mondayWork(hobby) {
+    if(hobby) {
+        return `This Monday, I will ${hobby}.`;
+    } else return "This Monday, I will go to the office.";
+}
+
+function wrapAdjective() {
+    return function(adjective) {
+        const adjective = "special";
+        return `You are ${adjective}`;
+    }
+}
